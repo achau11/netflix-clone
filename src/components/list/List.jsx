debffe60dd0e015a338d17ea3a7fcc69512dd541
ahmed <achau1019@gmail.com>
2021-09-21 19:45:29 -0400

    Finish sliding functionality

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -4,11 +4,16 @@ import ListItem from '../listItem/ListItem'
 import './list.scss'
 
 export default function List() {
+  const [isMoved, setIsMoved] = useState(false)
   const [slideNumber, setSlideNumber] = useState(0)
+  
   const listRef = useRef()
 
   const handleClick = (direction) => {
+    setIsMoved(true)
+
     let distance = listRef.current.getBoundingClientRect().x - 50
+
     if (direction === 'left' && slideNumber > 0) {
       setSlideNumber(slideNumber - 1);
       listRef.current.style.transform = `translateX(${230 + distance}px)`
@@ -23,7 +28,11 @@ export default function List() {
     <div className='list'>
       <span className="listTitle">Continue to watch</span>
       <div className="wrapper">
-        <ArrowBackIosOutlined className='sliderArrow left' onClick={() => handleClick('left')}/>
+        <ArrowBackIosOutlined 
+          className='sliderArrow left' 
+          onClick={() => handleClick('left')}
+          style={{display: !isMoved && 'none'}}
+        />
         <div className="container" ref={listRef}>
           <ListItem />
           <ListItem />
