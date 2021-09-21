56cdf403fa55dfde0a43fdca5a1f611a7d7907a4
ahmed <achau1019@gmail.com>
2021-09-21 13:48:18 -0400

    Make navbar

---
+++ src/home/Home.jsx
@@ -0,0 +1,13 @@
+import { AcUnit } from '@material-ui/icons'
+import Navbar from '../components/navbar/Navbar'
+import './home.scss'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default Home
